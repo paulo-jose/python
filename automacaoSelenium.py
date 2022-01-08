13def55da9849026cba9d3530632e38f24afe7a5
paulo <paulo.sousa.ti@gmail.com>
2022-01-08 12:13:30 -0300

    segundo commit

--- automacaoSelenium.py
+++ automacaoSelenium.py
@@ -6,6 +6,6 @@ navegador = webdriver.Chrome(ChromeDriverManager().install())
 navegador.get("https://www.google.com/")
 navegador.find_element_by_xpath('/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys("xvideos")
 navegador.find_element_by_xpath('/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys(Keys.ENTER)
-#navegador.quit()
+navegador.quit()
 
 
